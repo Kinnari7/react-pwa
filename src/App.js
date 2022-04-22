177531f16e17d162001a8522c575efaa6a2466ea
Kinnari7 <kinnari.ldrpit@gmail.com>
2022-04-22 16:48:12 +0530

    fix: first commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import './App.css';
+
+const NavBar = () => (
+  <div className="navbar">
+    <h3>Task Manager</h3>
+    <Link to="/">Current Tasks</Link>
+    <Link to="/completed">Completed Tasks</Link>
+  </div>
+);
+
+const Template = (props) => (
+  <div>
+    <NavBar />
+    <p className="page-info">
+      {props.title}:
+    </p>
+    <ul className={props.status}>
+      <li>Task 1</li>
+      <li>Task 2</li>
+      <li>Task 3</li>
+    </ul>
+  </div>
+);
+
+const CurrentTasks = () => (
+  <Template title="Current Tasks" status="Current" />
+);
+
+const CompletedTasks = () => (
+  <Template title="Completed Tasks" status="Completed" />
+);
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Test
+      </div>
+      // <Router>
+      //   <div>
+      //     <Route exact path="/" component={CurrentTasks} />
+      //     <Route path="/completed" component={CompletedTasks} />
+      //   </div>
+      // </Router>
+    );
+  }
+}
+
+export default App;
